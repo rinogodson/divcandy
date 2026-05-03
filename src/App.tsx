ca8991486967e38f19838fc4a7b51f05ae739d2f
Rino Godson <rinogodson@gmail.com>
2026-05-03 12:58:31 +0530

    made the basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="sm:w-[calc(100vw-5em)] gap-5 w-screen p-5 grid rounded-4xl grid-cols-[3fr_7fr] h-[calc(100vh-5em)] bg-[#1E2021] border border-x-0 sm:border-x border-white/20">
+      <div className="sm:h-full bg-[#191B1B] border border-white/20 rounded-xl"></div>
+
+      <div className="sm:h-full bg-[#191B1B] border border-white/20 rounded-xl"></div>
+    </div>
+  );
 };
 
 export default App;
